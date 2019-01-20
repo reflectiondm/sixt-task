b06af96474b8f2b3a2486fa51cea0468ea62f394
Andrei <reflection.dm@gmail.com>
2019-01-20 14:45:59 +0100

    Implement basic offers layout

--- app/components/OffersList.jsx
+++ app/components/OffersList.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const sixtOrange = '#ff5f00';
+
+const OffersGrid = styled.div`
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: auto;
+
+  @media screen and (min-width: 780px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+const OfferCardTileBox = styled.div`
+  padding-top: 100%;
+  height: 0;
+  width: 100%;
+  background: ${sixtOrange};
+  position: relative;
+`;
+
+const OfferCardTileLayout = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
 
 function OffersList({offers}) {
   return (
-    <div>{offers.length} offers</div>
+    <OffersGrid>{offers.map(offer => <OfferCard key={offer.id} offer={offer}/>)}</OffersGrid>
   );
 }
+
+function OfferCard({offer}) {
+  return (
+    <OfferCardTileBox>
+      <OfferCardTileLayout>
+        {offer.description}
+      </OfferCardTileLayout>
+    </OfferCardTileBox>
+  );
+}
+
+OfferCard.propTypes = {
+  offer: PropTypes.object.isRequired
+};
 
 OffersList.propTypes = {
   offers: PropTypes.array.isRequired
